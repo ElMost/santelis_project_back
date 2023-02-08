e1ff59ec8f2113359b2c22df7490eaa631177e93
Mouss <elmost777@gmail.com>
2023-02-08 17:03:28 +0100

    test git config

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,8 +22,4 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
-  rules: {
-    'linebreak-style': ['error', 'unix'],
-  },
-  
 };
