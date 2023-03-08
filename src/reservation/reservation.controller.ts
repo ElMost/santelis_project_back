d00b2effec2e766ec6804277c1e1745791090b2d
Mouss <elmost777@gmail.com>
2023-03-08 10:41:57 +0100

    maj

--- src/reservation/reservation.controller.ts
+++ src/reservation/reservation.controller.ts
@@ -6,32 +6,42 @@ import {
   Param,
   Delete,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { ReservationService } from './reservation.service';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
 import { Reservation } from './entities/reservation.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('reservation')
 export class ReservationController {
   constructor(private readonly reservationService: ReservationService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Post()
+  // Cette route permet de créer une reservation
   async create(@Body() createReservationDto: CreateReservationDto) {
     return this.reservationService.create(createReservationDto);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get()
+  // Cette route permet de récupérer toutes les reservations
   findAll() {
     return this.reservationService.findAll();
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get(':id')
+  // Cette route permet de récupérer une reservation en fonction de son id
   findOne(@Param('id') id: string) {
     return this.reservationService.findOne(id);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Put(':id')
+  // Cette route permet de mettre à jour une reservation en fonction de son id
   update(
     @Param('id') id: string,
     @Body() updateReservationDto: UpdateReservationDto,
@@ -39,11 +49,16 @@ export class ReservationController {
     return this.reservationService.update(id, updateReservationDto);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
+  // Cette route permet de supprimer une reservation en fonction de son id
   remove(@Param('id') id: string) {
     return this.reservationService.delete(id);
   }
+
+  @UseGuards(AuthGuard('jwt'))
   @Get('/by-email/:email')
+  // Cette route permet de récupérer toutes les reservations d'un utilisateur en fonction de son email
   async getReservationsByEmail(
     @Param('email') email: string,
   ): Promise<Reservation[]> {
--- src/reservation/reservation.service.ts
+++ src/reservation/reservation.service.ts
@@ -22,6 +22,12 @@ export class ReservationService {
         createReservationDto.email,
       );
 
+      if (!user) {
+        throw new NotFoundException(
+          `User with email ${createReservationDto.email} not found`,
+        );
+      }
+
       const newReservation = this.reservationRepository.create({
         ...createReservationDto,
         User: user,
@@ -40,23 +46,30 @@ export class ReservationService {
 
   async findAll() {
     try {
+      // Ce code permet de récupérer les reservations avec les infos de l'utilisateur
+      //qui a fait la reservation en utilisant la relation  OneToMany avec la table User
       const reservations = await this.reservationRepository.find({
         relations: ['User'],
       });
       console.log('reservations', reservations);
+      // si tout se passe bien, on renvoie les reservations
       return reservations;
     } catch (error) {
-      console.log('Erreur lors de la reservation', error);
+      // si une erreur survient, on la log et on renvoie un message d'erreur
       throw new Error('Impossible de récupérer les reservations');
     }
   }
 
   async findOne(id: string) {
     try {
+      // Ce code permet de récupérer les reservations avec les infos de l'utilisateur
+      //qui a fait la reservation en utilisant la relation  OneToMany avec la table User
       const reservation = await this.reservationRepository.findOne({
         where: { id },
         relations: ['User'],
       });
+      // si la reservation n'existe pas, on renvoie un message d'erreur
+      //  et un code d'erreur ( 404 = Not Found)
       if (!reservation) {
         return {
           message: `Reservation avec l'ID ${id} introuvable`,
@@ -68,6 +81,7 @@ export class ReservationService {
         return reservation;
       }
     } catch (error) {
+      // si une erreur survient, on la log et on renvoie un message d'erreur
       console.log('error', error);
       throw new Error(`Reservation avec l'ID ${id} introuvable`);
     }
@@ -75,22 +89,27 @@ export class ReservationService {
 
   async update(id: string, updateReservationDto: UpdateReservationDto) {
     try {
-      console.log('updateReservationDto', updateReservationDto);
+      // Ce code permet de récupérer les reservations avec les infos de l'utilisateur
+      //qui a fait la reservation en utilisant la relation  OneToMany avec la table User
       const reservation = await this.reservationRepository.findOne({
         where: { id },
         relations: ['User'],
       });
+      // si la reservation n'existe pas, on renvoie un message d'erreur
       if (!reservation) {
         throw new Error(`Reservation avec l'ID ${id} introuvable`);
       }
-      console.log('reservation', reservation);
+      // si la reservation existe, on met à jour les infos
+      // et on renvoie la reservation mise à jour
       const updatedReservation = await this.reservationRepository.save({
         ...reservation,
         ...updateReservationDto,
       });
+
       console.log('La reservation a été mise à jour', updatedReservation);
       return updatedReservation;
     } catch (error) {
+      // si une erreur survient, on la log et on renvoie un message d'erreur
       console.log('Erreur lors de la mise à jour de la reservation', error);
       throw new Error(`Reservation avec l'ID ${id} introuvable`);
     }
@@ -98,25 +117,31 @@ export class ReservationService {
 
   async delete(id: string) {
     try {
+      // Ce code permet de récupérer les reservations avec les infos de l'utilisateur
       const reservation = await this.reservationRepository.findOne({
         where: { id: id.toString() },
         relations: ['User'],
       });
       if (!reservation) {
+        // si la reservation n'existe pas, on renvoie un message d'erreur
         throw new Error(`Reservation avec l'ID ${id} introuvable`);
       }
       console.log('reservation', reservation);
+      // si la reservation existe, on la supprime
       const deletedReservation = await this.reservationRepository.delete({
         id,
       });
       console.log('La reservation a été supprimée', deletedReservation);
       return deletedReservation;
     } catch (error) {
+      // si une erreur survient, on la log et on renvoie un message d'erreur
       console.log('Erreur lors de la suppression de la reservation', error);
       throw new Error(`Reservation avec l'ID ${id} introuvable`);
     }
   }
+
   async getAllByEmail(email: string): Promise<Reservation[]> {
+    // Ce code permet de récupérer les reservations avec les infos de l'utilisateur
     return this.reservationRepository.find({ where: { email: email } });
   }
 }
